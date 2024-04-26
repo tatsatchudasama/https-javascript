9563b7c18977abe56fdbecb4a7ec91ad10cbc482
tatsatchudasama <chudasamatatsat@gmail.com>
2024-04-26 13:46:36 +0530

    web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -74,4 +83,12 @@ Route::get('/then-&-catch', function () {
 
 Route::get('/promise-API', function () {
     return view('promise_API');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('welcome');
 });
