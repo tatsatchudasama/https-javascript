c9e98d43a9df81b6bc230a1f3b46dea458b7a32e
tatsatchudasama <chudasamatatsat@gmail.com>
2024-04-29 17:25:21 +0530

    insertAdjacentHTML text

---
+++ public/js/insertAdjacentHTML.js
@@ -0,0 +1,24 @@
+
+
+
+// element position set 
+
+// Insert HTML text before the container div
+first.insertAdjacentHTML('beforebegin', '<h2>New beforebegin</h2>');
+
+
+// Insert HTML text as the last child of the container div
+first.insertAdjacentHTML('beforeend', '<h2>New beforeend</h2>');
+
+
+// Insert HTML text as the first child of the container div
+first.insertAdjacentHTML('afterbegin', '<h2>New afterbegin</h2>');
+
+
+// Insert HTML text after the container div
+first.insertAdjacentHTML('afterend', '<h2>New afterend</h2>');
+
+
+
+// remove a element
+remove.remove();
